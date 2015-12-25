a8b95d9d6c524b4863be29449c7420ffe27371e8
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-25 04:45:48 +0000

    Added functionality in JS so that it would diaply inputted to do list

--- list.js
+++ list.js
@@ -1,9 +1,18 @@
  var myDataRef = new Firebase('https://popping-heat-1392.firebaseio.com/');
  $('#ToDoInput').keypress(function (e) {
-        if (e.keyCode == 13) {
-          var name = $('#nameInput').val();
-          var todo = $('#ToDoInput').val();
-          myDataRef.push({name: name, todo: todo});
-          $('#ToDoInput').val('');
+     if (e.keyCode == 13) {
+        var name = $('#nameInput').val();
+        var todo = $('#ToDoInput').val();
+        myDataRef.push({name: name, todo: todo});
+        $('#ToDoInput').val('');
         }
-      });
+     });
+    myDataRef.on('child_added', function(snapshot) {
+        var listItem = snapshot.val();
+		displayToDoList(listItem.name, listItem.todo);
+    });
+    
+    function displayToDoList(name, todo) {
+        $('<div/>').text(todo).prepend($('<em/>').text(name+': ')).appendTo($('#listDiv'));
+        $('#listDiv')[0].scrollTop = $('#listDiv')[0].scrollHeight;
+      };
